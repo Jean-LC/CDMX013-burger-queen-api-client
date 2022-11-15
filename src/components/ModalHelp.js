d7215f6589e4515085f6c7322ca0ee17000062b8
laladiaz <mariaabelardadiaz@gmail.com>
2022-11-15 16:58:52 -0600

    feat: creates the modal component for help in ModalHelp.js

--- src/components/ModalHelp.js
+++ src/components/ModalHelp.js
@@ -1,18 +1,31 @@
+import './ModalHelp.css';
+import logoModal from '../images/texto-suchito.png';
 
-function ModalHelp() {
-    return(
-        <div className= "modal-help">
-            <dialog className= "modal-help-dialog">
-                <p>
-                If you need help with user / password contact the <span className="admin-help">admin</span>
-                </p>
-                <p>
-                If you are the admin, contact <span className="development-help">development</span>
-                </p>
-                <button className="close-help">
-                    Close
-                </button>
-            </dialog>
+function ModalHelp({ show, onClose }) {
+    if (!show) {
+        return null;
+    }
+    return (
+        <div className="modal-help" onClick={onClose}>
+            <div className="modal-help-content" onClick={(e) => e.stopPropagation()}>
+                <header className="modal-header">
+                    <img src={logoModal} className="logo-modal" alt="logo"></img>
+                </header>
+
+                <body className="modal-body">
+                    <p>
+                        If you need help with user / password contact the <span id="admin-help">admin</span>
+                    </p>
+                    <p>
+                        If you are the admin, contact <span id="development-help">development</span>
+                    </p>
+                </body>
+                <footer className="modal-footer">
+                    <button className="close-help" onClick={onClose}>
+                        Close
+                    </button>
+                </footer>
+            </div>
         </div>
     );
 }
