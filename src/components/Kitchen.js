32dde4868c12ccf1cd95518eb89f6b58e35d5a91
Jean-LC <jeanettelc@outlook.com>
2022-11-21 14:09:25 -0600

    new: creates the Kitchen component in Kitchen.js”

---
+++ src/components/Kitchen.js
@@ -0,0 +1,12 @@
+import './styles/grid.css';
+
+const Kitchen = () => {
+    return (
+        <article className= "grid">
+            <h2 className="missing-page-h2">Kitchen </h2>
+        
+        </article>
+    )
+}
+
+export default Kitchen
