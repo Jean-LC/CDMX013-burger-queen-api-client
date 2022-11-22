ab48fb635895c74109234992cad76f4ed23c979e
menamilochomil <jimenamilochomil@gmail.com>
2022-11-22 13:45:21 -0600

    feat: adds delete function in Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,20 +1,31 @@
 import './styles/Users.css';
 import useAuth from "../hook/useAuth";
 import { useEffect, useState } from 'react';
-import { getUsers } from '../services/api';
+import { getUsers, deleteUsers } from '../services/api';
 import edit from '../images/edit.png';
 import deleteImg from '../images/deleteImg.png';
 
 const Users = () => {
     const { auth } = useAuth();
     const [data, setData] = useState([]);
+    // const [userDelete, setUserDelete] = useState([])}
+
     const readUser = async () => {
         try {
-            const {data} = await getUsers(auth.accessToken) 
+            const { data } = await getUsers(auth.accessToken)
             setData(data)
         }
         catch (err) {
             console.log(err.response.data)
+        }
+    }
+    const dltUsers = async (userId) => {
+        try {
+            const dlt = await deleteUsers(userId, auth.accessToken)
+            console.log(setData(data.filter((user) => user.id !== userId)))
+
+        } catch (err) {
+
         }
     }
 
@@ -22,13 +33,27 @@ const Users = () => {
         readUser()
     }, [])
 
+/*      useEffect(() =>{
+        dltUsers(data.id)
+    }, [])  */
+
     return (
         <>
             {data.map((user) => (
                 <div className="div-users" key={user.id}>
-                    <p >{user.role}   {user.email}</p> 
-                    <img src={edit} className='edit-button' alt='edit-button'></img>
-                    <img src={deleteImg} className='delete-image' alt='delete-image'></img>
+                    <p >{user.role}</p> 
+                    <p>{user.email}</p>
+                    <img src={edit} className='edit-button'
+                        alt='edit-button'>
+                    </img>
+                    <img src={deleteImg}
+                        className='delete-image'
+                        alt='delete-image'
+                        id={user.id}
+                        onClick={() => dltUsers(user.id)}
+                        /* onChange={() => setData(dltUsers)} */
+                    >
+                    </img>
                 </div>
             ))}
         </>
