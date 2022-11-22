22e47add7306aba6c78c4be9c3858b91ff97f956
menamilochomil <jimenamilochomil@gmail.com>
2022-11-22 12:29:58 -0600

    feat: create axios function to get and post a user in services folder

--- src/services/api.js
+++ src/services/api.js
@@ -2,13 +2,18 @@ import axios from "axios";
 
 const BASE_URL = 'http://localhost:8080'
 
+export const loginUser =  (userEmail, userPassword) => {
+    return  axios.post( `${BASE_URL}/login`, {
+    email: userEmail,
+    password: userPassword
+  })
+}
+
 export const getUsers = (token) => {
     return axios.get(`${BASE_URL}/users`, {
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`
         }
-    }
-    );
-
+    });
 }
