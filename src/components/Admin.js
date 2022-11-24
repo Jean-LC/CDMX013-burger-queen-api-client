f0d88126ccf1ea6c86c8fecf2f281e53a296c402
laladiaz <mariaabelardadiaz@gmail.com>
2022-11-23 18:26:58 -0600

    change: changes the functions from Users and ModalUser to Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './styles/grid.css';
 import './styles/Admin.css'
 import HeaderGeneral from "./HeaderGeneral.js";
@@ -6,30 +6,56 @@ import NavbarAdmin from "./NavbarAdmin.js";
 import useAuth from "../hook/useAuth.js";
 import ModalUser from './ModalUser.js';
 import Users from './Users';
+import { getUsers, createUser } from '../services/api';
 
 const Admin = () => {
-const [show, setShow] = useState(false);
-const { auth } = useAuth();
-// ¿Queremos que la App/admin no aparezca cuando refresquen la pag?
-        return (
-            <div className="grid">
-                <header className='admin-header'>
-                <HeaderGeneral section={'ADMIN'} email={auth.user.email}/>
-                </header>
-                    <div className='nav-bar-admin'>
-                    <NavbarAdmin />
-                    </div>
-                    <div className='button-create-user'>
-                    <button className="create-user" onClick={() => setShow(true)}>Create user</button>
-                    </div>
-                <section className='body-admin'>
-                    <ModalUser onClose={() => setShow(false)} show={show}/>
-                    <div className='users-rendering'>
-                        <Users />
-                    </div>
-                </section>
+    const [show, setShow] = useState(false);
+    const { auth } = useAuth();
+    const [dataUser, setDataUser] = useState([]);
+
+    const readUser = async () => {
+        try {
+            const { data } = await getUsers(auth.accessToken)
+            setDataUser(data)
+        }
+        catch (err) {
+            console.log(err.response.data)
+        }
+    }
+
+    const addUser = async (newUser) => {
+        try {
+            await createUser(newUser);
+            readUser();
+
+        } catch (err) {
+            console.log(err.response.data)
+        }
+    }
+
+    useEffect(() => {
+        readUser()
+    }, [])
+
+    return (
+        <div className="grid">
+            <header className='admin-header'>
+                <HeaderGeneral section={'ADMIN'} email={auth.user.email} />
+            </header>
+            <div className='nav-bar-admin'>
+                <NavbarAdmin />
             </div>
-        );
+            <div className='button-create-user'>
+                <button className="create-user" onClick={() => setShow(true)}>Create user</button>
+            </div>
+            <section className='body-admin'>
+                <ModalUser onClose={() => setShow(false)} show={show} onSubmit={addUser}/>
+                <div className='users-rendering'>
+                    <Users users={dataUser} />
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default Admin;
