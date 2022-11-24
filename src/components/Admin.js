c778e0e70818f830743c04470d69554ae0b2fa37
Jean-LC <jeanettelc@outlook.com>
2022-11-24 16:46:00 -0600

    change: moves delete function, adds the errors to add user in admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -6,12 +6,13 @@ import NavbarAdmin from "./NavbarAdmin.js";
 import useAuth from "../hook/useAuth.js";
 import ModalUser from './ModalUser.js';
 import Users from './Users';
-import { getUsers, createUser } from '../services/api';
+import { getUsers, createUser, deleteUsers } from '../services/api';
 
 const Admin = () => {
     const [show, setShow] = useState(false);
     const { auth } = useAuth();
     const [dataUser, setDataUser] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const readUser = async () => {
         try {
@@ -27,10 +28,24 @@ const Admin = () => {
         try {
             await createUser(newUser);
             readUser();
+            setShow(false)
+            setErrorMessage('')
+        } catch (err) {
+            setErrorMessage(err.response.data)
+        }
+    }
+    const hadleDltUsers = async (userId) => {
+       try {
+            const dlt = await deleteUsers(userId, auth.accessToken);
+            console.log('dlt', dlt)
+            const newData = dataUser.filter((item) => item.id !== userId)
+
+            console.log("funciona?", newData)
+            setDataUser(newData)
 
         } catch (err) {
-            console.log(err.response.data)
-        }
+            console.log(err)
+        } 
     }
 
     useEffect(() => {
@@ -49,9 +64,13 @@ const Admin = () => {
                 <button className="create-user" onClick={() => setShow(true)}>Create user</button>
             </div>
             <section className='body-admin'>
-                <ModalUser onClose={() => setShow(false)} show={show} onSubmit={addUser}/>
+                <ModalUser onClose={() => {setShow(false)
+                setErrorMessage('')}} 
+                show={show} 
+                onSubmit={addUser} 
+                error={errorMessage}/>
                 <div className='users-rendering'>
-                    <Users users={dataUser} />
+                    <Users users={dataUser} handleDelete={hadleDltUsers}/>
                 </div>
             </section>
         </div>
