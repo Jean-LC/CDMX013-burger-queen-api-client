7d2a6ad8d2ffccdab5c23ee8e5ca1194b0d8208e
Menamilochomil <jimenamilochomil@gmail.com>
2022-11-25 18:07:05 -0600

    refactor: rewrites the functions to optimize their use in Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -6,17 +6,22 @@ import NavbarAdmin from "./NavbarAdmin.js";
 import useAuth from "../hook/useAuth.js";
 import ModalUser from './ModalUser.js';
 import Users from './Users';
-import { getUsers, createUser, deleteUsers } from '../services/api';
+import { axiosGet, axiosPost, axiosDelete, axiosPatch } from '../services/api';
 
 const Admin = () => {
     const [show, setShow] = useState(false);
     const { auth } = useAuth();
     const [dataUser, setDataUser] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
+    const [oneUser, setOneUser] = useState({
+        email: '',
+        role: ''
+    })
+    const URL_USERS = '/users'
 
     const readUser = async () => {
         try {
-            const { data } = await getUsers(auth.accessToken)
+            const { data } = await axiosGet(auth.accessToken, URL_USERS)
             setDataUser(data)
         }
         catch (err) {
@@ -26,7 +31,7 @@ const Admin = () => {
 
     const addUser = async (newUser) => {
         try {
-            await createUser(newUser);
+            await axiosPost(newUser, URL_USERS);
             readUser();
             setShow(false)
             setErrorMessage('')
@@ -35,8 +40,8 @@ const Admin = () => {
         }
     }
     const hadleDltUsers = async (userId) => {
-       try {
-            const dlt = await deleteUsers(userId, auth.accessToken);
+        try {
+            const dlt = await axiosDelete(userId, auth.accessToken, URL_USERS);
             console.log('dlt', dlt)
             const newData = dataUser.filter((item) => item.id !== userId)
 
@@ -45,11 +50,21 @@ const Admin = () => {
 
         } catch (err) {
             console.log(err)
-        } 
+        }
     }
 
-    const editUser = (id) => {
-        console.log("holis soy edit", id)
+    const editUser = async (id) => {
+        try {
+            setOneUser((await axiosGet(auth.accessToken, `${URL_USERS}/${id}`)).data);
+            setShow(true)
+            console.log(oneUser);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const patchUser = () => {
+        
     }
 
     useEffect(() => {
@@ -68,13 +83,20 @@ const Admin = () => {
                 <button className="create-user" onClick={() => setShow(true)}>Create user</button>
             </div>
             <section className='body-admin'>
-                <ModalUser onClose={() => {setShow(false)
-                setErrorMessage('')}} 
-                show={show} 
-                onSubmit={addUser} 
-                error={errorMessage}/>
+                <ModalUser onClose={() => {
+                    setShow(false)
+                    setErrorMessage('')
+                    setOneUser({
+                        email: '',
+                        role: ''
+                    })
+                }}
+                    show={show}
+                    onSubmit={addUser}
+                    error={errorMessage}
+                    user={oneUser} />
                 <div className='users-rendering'>
-                    <Users users={dataUser} handleDelete={hadleDltUsers} handleUser={editUser}/>
+                    <Users users={dataUser} handleDelete={hadleDltUsers} handleUser={editUser} />
                 </div>
             </section>
         </div>
