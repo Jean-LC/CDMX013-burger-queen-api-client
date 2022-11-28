b033c4e7fc6fafc72c042a53ba20a10ccc1f9cba
Menamilochomil <jimenamilochomil@gmail.com>
2022-11-28 09:42:38 -0600

    feat: adds the second view of Admin in NavbarAdmin.js

--- src/components/NavbarAdmin.js
+++ src/components/NavbarAdmin.js
@@ -1,11 +1,12 @@
 import './styles/NavbarAdmin.css';
+import {NavLink} from 'react-router-dom'
 
 const NavbarAdmin = () => {
     return (
         <nav className='navbar-admin'>
             <div className='navbar-admin-content'>
-                 <button className='user-management'>User management</button>
-                 <button className='product-management'>Product management</button>
+                <NavLink to='/admin' activeClassName='active' className='user-management'>User management</NavLink>
+                <NavLink to='/product-management' activeClassName='active' className='product-management'>Product management</NavLink>
             </div>
         </nav>
     )
