318135b224d794318645254410f2200527206172
Menamilochomil <jimenamilochomil@gmail.com>
2022-11-28 09:41:29 -0600

    feat: creats the second view to Admin in ProductAdmin.js

---
+++ src/components/ProductAdmin.js
@@ -0,0 +1,20 @@
+import HeaderGeneral from "./HeaderGeneral.js";
+import NavbarAdmin from "./NavbarAdmin.js";
+const ProductAdmin = () => {
+    return (
+        <div className="grid">
+            <header className='admin-header'>
+                <HeaderGeneral section={'ADMIN'} email={"mena@suchito.com"} />
+            </header>
+            <div className='nav-bar-admin'>
+                <NavbarAdmin />
+            </div>
+            <div className='button-create-product'>
+                <button className="create-product">Create product</button>
+            </div>     
+        </div>
+        
+    )
+}
+
+export default ProductAdmin
