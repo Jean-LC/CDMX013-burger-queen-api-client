079532b651564c9879a29b795abcd1d6fb93dcb0
laladiaz <mariaabelardadiaz@gmail.com>
2022-11-28 12:59:57 -0600

    change: changes the activeclassaname attribute to all lowercase in NavbarAmdmin.js

--- src/components/NavbarAdmin.js
+++ src/components/NavbarAdmin.js
@@ -5,8 +5,8 @@ const NavbarAdmin = () => {
     return (
         <nav className='navbar-admin'>
             <div className='navbar-admin-content'>
-                <NavLink to='/admin' activeClassName='active' className='user-management'>User management</NavLink>
-                <NavLink to='/product-management' activeClassName='active' className='product-management'>Product management</NavLink>
+                <NavLink to='/admin' activeclassname='active' className='user-management'>User management</NavLink>
+                <NavLink to='/product-management' activeclassname='active' className='product-management'>Product management</NavLink>
             </div>
         </nav>
     )
