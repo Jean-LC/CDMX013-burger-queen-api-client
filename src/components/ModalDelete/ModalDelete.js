fc609168c1f3ae520280ed8d8f0dd43990ab6fbd
Jean-LC <jeanettelc@outlook.com>
2022-11-27 19:28:05 -0600

    style: gives style to modal delete in modalDelete.js

--- src/components/ModalDelete/ModalDelete.js
+++ src/components/ModalDelete/ModalDelete.js
@@ -4,16 +4,18 @@ import confirmImg from "../../images/check.png"
 // import { closeModal } from "./showModal";
 
 
-const ModalDelete = ({show, deleteHandle, onClose}) => {
+const ModalDelete = ({ show, deleteHandle, onClose }) => {
     if (!show) {
         return null;
     }
     return (
         <div className="delete-modal" >
             <div className='delete-modal-content'>
-            Are you sure you want to delete?
-            <img src={confirmImg} alt='confirm' className="img-delete-modal" onClick={deleteHandle}></img>
-            <img src={deleteImg} alt='delete' className="img-delete-modal" onClick={onClose}></img>
+                <p className='paragraph-confirmation'> ARE YOU SURE YOU WANT TO DELETE? </p>
+                <div className='buttons-delete'>
+                    <img src={confirmImg} alt='confirm' className="img-delete-modal" onClick={deleteHandle}></img>
+                    <img src={deleteImg} alt='delete' className="img-delete-modal" onClick={onClose}></img>
+                </div>
             </div>
         </div>
     );
