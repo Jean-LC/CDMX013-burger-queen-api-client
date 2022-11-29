7cc99b3c3f6541237dc9dfefe9d5cd043d57a63b
Jean-LC <jeanettelc@outlook.com>
2022-11-29 11:34:26 -0600

    new: starts with modal help tests (doesn't work)

--- src/test/ModalHelp.test.js
+++ src/test/ModalHelp.test.js
@@ -1,3 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import ModalHelp from '../components/ModalHelp';
 
+describe('Modal help rendering tests', () => {
+
+    test('the logo is in the modal', () => {
+        render(<ModalHelp />);
+        const logoImg = screen.getByRole('Button');
+        expect(logoImg).toBeInTheDocument();
+
+    })
+
+})
