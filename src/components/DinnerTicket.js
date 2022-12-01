b1cac5c3d5196394e6f1533532bdf1607331f35d
Menamilochomil <jimenamilochomil@gmail.com>
2022-12-01 13:08:06 -0600

    feat: adds the map function to get the products in DinnerTicket.js

--- src/components/DinnerTicket.js
+++ src/components/DinnerTicket.js
@@ -7,14 +7,17 @@ const DinnerTicket = ({name, products}) => {
     return(
         <div className= "dinner-ticket-container">
                 <p className= "dinner-ticket-p">Client: {name}</p>
-            <div className= "ticket-main">
+                {products.map((product) => (
+            <div className= "ticket-main" key={product.id}>
                 <section className="btn-plus-minus-dinner">
                     <img  src={rest} className='rest' alt='rest'></img>
                     <p className="dinner-counter">0</p>
                     <img src={add} className='rest' alt='rest'></img>
                 </section>
-                <p className= "dinner-ticket-list">Hay que hacer un map $10</p>
+                <p className= "dinner-ticket-list">{product.name}  ${product.price}</p>
             </div>
+                ))
+                }
             <div>
                 <p className='total-ticket'>Total: </p>
                 <button className='reset-kitchen-button'>Reset</button>
