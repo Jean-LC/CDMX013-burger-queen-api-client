1bf3578e693b4c0a7dae657d691d261849a04707
laladiaz <mariaabelardadiaz@gmail.com>
2022-12-04 16:04:47 -0600

    style: gives style to the DinnerTicket component

--- src/components/DinnerTicket.js
+++ src/components/DinnerTicket.js
@@ -6,27 +6,29 @@ import rest from '../images/rest.png'
 const DinnerTicket = ({ name, products, addProduct, lessProduct, reset, send }) => {
     let total = null;
     return (
-        <div className="dinner-ticket-container">
-            <p className="dinner-ticket-p">Client: {name}</p>
-            {products && products.map((product) => (
-                <div className="ticket-main" key={product.product.id}>
-                    <section className="btn-plus-minus-dinner">
-                        <img src={rest} className='rest' alt='rest' onClick={() => lessProduct(product.product)}></img>
-                        <p className="dinner-counter">{product.qty}</p>
-                        <img src={add} className='rest' alt='add' onClick={ () => addProduct(product.product)}></img>
-                    </section>
-                    <p className="dinner-ticket-name">{product.product.name}</p>
-                    <p className="dinner-ticket-price"> ${product.product.price}</p>
-                    <p className='total-ticket'> $ {product.product.price * product.qty}</p>
-                    <span className='just-total' >{total += product.product.price * product.qty}</span>
-                </div>
-
-            ))
-            }
-            <div>
-                {total && <p className='total-ticket'> Total: $ {total}</p>}
+        <div>
+            <div className="dinner-ticket-container">
+                <p className="dinner-ticket-client">Client: {name}</p>
+                {products && products.map((product) => (
+                    <div className="ticket-main" key={product.product.id}>
+                        <section className="btn-plus-minus-dinner">
+                            <img src={rest} className='rest' alt='rest' onClick={() => lessProduct(product.product)}></img>
+                            <p className="dinner-counter">{product.qty}</p>
+                            <img src={add} className='rest' alt='add' onClick={() => addProduct(product.product)}></img>
+                        </section>
+                        <div className='dinner-ticket-ps'>
+                            <p className="dinner-ticket-name">{product.product.name}</p>
+                            <p className="dinner-ticket-price"> ${product.product.price}</p>
+                            <p className='total-ticket'> $ {product.product.price * product.qty}</p>
+                        </div>
+                        <span className='just-total' >{total += product.product.price * product.qty}</span>
+                    </div>
+                ))}
+            </div>
+            <div className='dinner-ticket-buttons'>
                 <button className='reset-kitchen-button' onClick={reset}>Reset</button>
-                <button className='send-kitchen-button' onClick={ () => send}>Send to Kitchen</button>
+                {total && <p className='total-ticket'> Total: $ {total}</p>}
+                <button className='send-kitchen-button' onClick={() => send}>Send to Kitchen</button>
             </div>
         </div>
     );
