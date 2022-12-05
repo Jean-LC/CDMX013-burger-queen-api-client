790778ecccbf0564ddf37063ad84eec6c73a1c50
laladiaz <mariaabelardadiaz@gmail.com>
2022-12-05 09:04:53 -0600

    resolves the pull upstream problems

---
+++ src/components/ModalTicket.js
@@ -0,0 +1,39 @@
+import './styles/ModalTicket.css'
+
+const ModalTicket = ({ id, nameStaff, comanda, nameClient, show, onClose, send, reset }) => {
+    // console.log("soy desde modal tickt", comanda[0])
+    let total = null
+    if (!show) {
+        return null
+    } else {
+        return (
+            <div className='modal-ticket' >
+                <div className="modal-ticket-content" >
+                    <div>
+                        <p>SUSHI MENU</p>
+                        <p>TICKET ID: {id} </p>
+                        <p>DINNER STAFF: {nameStaff}</p>
+                        <p>CLIENT: {nameClient} </p>
+                    </div>
+                    {comanda[0].products.map((product) => {
+                        return (
+                            <div key={product.product.id}>
+                                <p className="comanda">
+                                    {product.qty}
+                                    {product.product.name}
+                                    ${product.qty *product.product.price}</p>
+                                <span>{total += product.qty * product.product.price}</span>
+                            </div>
+                        )
+                    })}
+                    <p>Total: ${total}</p>
+                    <button onClick={() => send()}>Send to kitchen</button>
+                    <button onClick={onClose} >Cancel</button>
+                </div >
+            </div>
+        )
+    }
+
+}
+
+export default ModalTicket;
