863f9847dc60623db647d41601f0285183e3e980
Menamilochomil <jimenamilochomil@gmail.com>
2022-12-06 16:41:51 -0600

    feat: gives the funtionality to Open bar CRUD in DinnerOpenBar.js

--- src/components/Views/DinnerOpenBar.js
+++ src/components/Views/DinnerOpenBar.js
@@ -1,26 +1,123 @@
 import '../styles/grid.css';
-import '../styles/DinnerOpenBar.css'
+import '../styles/DinnerSuchiMenu.css'
 import useAuth from '../../hook/useAuth';
 import HeaderGeneral from '../HeaderGeneral';
 import NavBarDinner from '../NavBarDinner.js';
-import suchihappy from '../../images/suchi-happy.png'
+import GridProductDinner from '../GridProductDinner.js';
+import DinnerTicket from '../DinnerTicket';
+import ModalTicket from '../ModalTicket';
 
+import { axiosGet, axiosPost } from '../../services/api';
+import { useState, useEffect } from 'react';
 
 const DinnerOpenBar = () => {
-    const {auth} = useAuth()
+    const { auth } = useAuth()
+    const [show, setShow] = useState(false)
+    const [dataMenu, setDataMenu] = useState([])
+    const [client, setClient] = useState('')
+    const [productsOrder, setProductsOrder] = useState([])
+    const order =
+    {
+        "id": Date.now(),
+        "userId": auth.user.id,
+        "client": client,
+        "products": productsOrder,
+        "status": 'pending',
+        "dataEntry": Date().toString()
+    }
+
+    const URL_PRODUCTS = '/products'
+    const URL_ORDERS = '/orders'
+
+    const readProducts = async () => {
+        try {
+            const data = await axiosGet(auth.accessToken, URL_PRODUCTS)
+            setDataMenu(data.data)
+        } catch (err) {
+            console.log(err.response.data);
+        }
+    }
+
+    const openBarMenu = dataMenu.filter((item) => item.type === 'Open bar')
+
+    const handleProducts = (product) => {
+        let arrayIndex = productsOrder.findIndex((a) => a.product.name === product.name)
+        if (arrayIndex < 0) {
+            setProductsOrder([...productsOrder, { qty: 1, product: product }]);
+        }
+        else {
+            let newOrder = productsOrder;
+            newOrder[arrayIndex].qty++
+            setProductsOrder([...newOrder]);
+        }
+    }
+
+    const lessProduct = (product) => {
+        let indexProduct = productsOrder.findIndex((a) => a.product.name === product.name)
+        let newOrder = productsOrder;
+        if (newOrder[indexProduct].qty === 1) {
+            newOrder = newOrder.filter((elements) => elements.product.name !== product.name)
+            setProductsOrder([...newOrder]);
+        } else {
+            newOrder[indexProduct].qty--
+            setProductsOrder([...newOrder]);
+        }
+    }
+
+    const sendOrder = async () => {
+        try {
+            const orderAxios = await axiosPost(order, URL_ORDERS, auth.accessToken);
+            setClient('')
+            setProductsOrder([])
+            document.getElementById('client').value = ''
+            setShow(false)
+        } catch (err) {
+            console.log(err.response.data);
+        }
+    }
+    const reset = () =>{
+        setProductsOrder([])
+        setClient('')
+        document.getElementById('client').value = ''
+    }
+
+    useEffect(() => {
+        readProducts()
+    }, [])
+
     return (
-        <article className= "grid">
-            <header className='dinner-open-bar-header'>
-                <HeaderGeneral section={auth.user.role.toUpperCase()} email={auth.user.email}/>
+        <article className="grid">
+            <header className='dinner-sushi-menu-header'>
+                <HeaderGeneral section={auth.user.role.toUpperCase()} email={auth.user.email} />
             </header>
             <div className='nav-bar-dinner'>
-            <NavBarDinner />
+                <NavBarDinner />
             </div>
-            <img src={suchihappy} alt="sushito-happy" className="suchito-happy"></img>
-            <p>Under construction</p>
-        
+            <article className='products'>
+                <GridProductDinner products={openBarMenu}
+                    setClient={setClient}
+                    newTicket={handleProducts} />
+
+            </article>
+            <article className='ticket'>
+                <DinnerTicket name={client}
+                    products={productsOrder}
+                    addProduct={handleProducts}
+                    lessProduct={lessProduct}
+                    reset={reset}
+                    show={() => setShow(true)}
+                />
+            </article>
+            <ModalTicket id={order.id}
+                nameStaff={auth.user.email}
+                nameClient={client}
+                comanda={order}
+                show={show}
+                onClose={() => setShow(false)}
+                send={sendOrder}
+            />
         </article>
     )
 }
 
-export default DinnerOpenBar
+export default DinnerOpenBar;
